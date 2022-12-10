3a128b45896b02336a309b1e02f743a45a8ae29f
jaime <alu0101329093@ull.edu.es>
2022-12-10 19:26:00 +0000

    Add Symbol

---
+++ include/turing_machine/symbol.h
@@ -0,0 +1,54 @@
+#ifndef P2TURINGMACHINE_TURING_MACHINE_SYMBOL_H_
+#define P2TURINGMACHINE_TURING_MACHINE_SYMBOL_H_
+
+#include <cstddef>
+#include <functional>
+#include <istream>
+
+namespace cc {
+
+class Symbol {
+ public:
+  Symbol(char symbol = ' ');
+
+  inline char Get() const { return symbol_; }
+  inline void Set(char new_symbol) { symbol_ = new_symbol; }
+
+  friend bool operator==(const Symbol& first_symbol,
+                         const Symbol& second_symbol) {
+    return first_symbol.symbol_ == second_symbol.symbol_;
+  }
+  friend bool operator!=(const Symbol& first_symbol,
+                         const Symbol& second_symbol) {
+    return first_symbol.symbol_ != second_symbol.symbol_;
+  }
+  friend bool operator<(const Symbol& first_symbol,
+                        const Symbol& second_symbol) {
+    return first_symbol.symbol_ < second_symbol.symbol_;
+  }
+  friend bool operator>(const Symbol& first_symbol,
+                        const Symbol& second_symbol) {
+    return first_symbol.symbol_ > second_symbol.symbol_;
+  }
+
+  friend std::istream& operator>>(std::istream& is, Symbol& symbol) {
+    char name{};
+    is >> name;
+    symbol.symbol_ = name;
+    return is;
+  }
+
+ private:
+  char symbol_;
+};
+
+}  // namespace cc
+
+template <>
+struct std::hash<cc::Symbol> {
+  std::size_t operator()(const cc::Symbol& symbol) const noexcept {
+    return std::hash<char>{}(symbol.Get());
+  }
+};
+
+#endif  // P2TURINGMACHINE_TURING_MACHINE_SYMBOL_H_
---
+++ src/symbol.cpp
@@ -0,0 +1,7 @@
+#include "turing_machine/symbol.h"
+
+namespace cc {
+
+Symbol::Symbol(char symbol) : symbol_{symbol} {}
+
+}  // namespace cc
